3c69acf48cce68146e55edf92ff942b35421f9c9
Dilan Grajales <193075@uptapachula.edu.mx>
2022-04-10 19:26:32 -0500

    paths

--- apps/authentication/urls.py
+++ apps/authentication/urls.py
@@ -6,16 +6,10 @@ urlpatterns = [
     path('login/', login_view, name="login"),
     path('register/', register_user, name="register"),
     path("logout/", LogoutView.as_view(), name="logout"),
-    re_path(r'^reset/password_reset', PasswordResetView.as_view(), {
-        'template_name': 'registration/password_reset_form.html',
-        'email_template_name': 'registration/password_reset_email.html'}, name="password_reset"
-    ),
-    re_path(r'^reset/password_reset_done', PasswordResetDoneView.as_view(), {
-        'template_name': 'registration/password_reset_done.html'}, name="password_reset_done"
-    ),
+    path('password_reset/', PasswordResetView.as_view(), name="password_reset"),
+    path('password_reset_done/', PasswordResetDoneView.as_view(), name="password_reset_done"),
     re_path(r'^reset/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$', PasswordResetConfirmView.as_view(), {
-        'template_name': 'registration/password_reset_confirm.html'},name='password_reset_confirm'),
-    re_path(r'^reset/done', PasswordResetCompleteView.as_view(), {
-        'template_name': 'registration/password_reset_complete.html'}, name="password_reset_complete"
+        'template_name': 'registration/password_reset_confirm.html'},name='password_reset_confirm'
     ),
+    path('done/', PasswordResetCompleteView.as_view(), name="password_reset_complete"),
 ]
