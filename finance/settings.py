6b7b3925b66dc052d5d9e208e5ce72b12494fc9c
Dilan Grajales <193075@uptapachula.edu.mx>
2022-04-12 23:59:18 -0500

    permitir conexiones

--- finance/settings.py
+++ finance/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = 'django-insecure-hw=c+7pb!4js_n2b4p8yq)c-m+2g_b^hrl=+#uz#x8rob+9r3=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
